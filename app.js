db16ca48972acfc2305928da61db8cc466e6fc34
JDon492 <joshdonovan492@gmail.com>
2022-07-09 16:31:12 +0100

    Functionality to new books and grid updates

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ class Library {
     }
   
     addBook(newBook) {
-      if (!this.isInLibrary(newBook)) {
+      if (!this.alreadyExists(newBook)) {
         this.books.push(newBook)
       }
     }
@@ -34,11 +34,11 @@ class Library {
       this.books = this.books.filter((book) => book.title !== title)
     }
   
-    getBook(title) {
+    findBook(title) {
       return this.books.find((book) => book.title === title)
     }
   
-    isInLibrary(newBook) {
+    alreadyExists(newBook) {
       return this.books.some((book) => book.title === newBook.title)
     }
   }
@@ -47,7 +47,7 @@ class Library {
 
 // UI
 
-// New Book Button & Form
+// New Book Button
 
 const newBookForm = () => {
     form.reset()
@@ -63,3 +63,85 @@ const closeForm = () => {
 }
 
 docOverlay.onclick = closeForm
+
+// New Book Form
+
+const bookFormInput = () => {
+    const title = document.getElementById("get-title").value
+    const author = document.getElementById("get-author").value
+    const pages = document.getElementById("get-pages").value
+    const readStatus = document.getElementById("get-read-status").checked
+    return new Book(title,author,pages,readStatus)
+}
+
+const addBook = (e) => {
+    e.preventDefault()
+    const newBook = bookFormInput()
+
+    if (library.alreadyExists(newBook)) {
+        errorMsg.textContent = "This book already exists"
+        return
+    }
+
+    library.addBook(newBook)
+
+    updateGrid()
+    closeForm()
+}
+
+form.onsubmit = addBook
+
+// Container Updates
+
+const clearGrid = () => {
+    mainCont.innerHTML = ""
+}
+
+const updateGrid = () => {
+    clearGrid()
+    for (let book of library.books) {
+        createBookTile(book)
+    }
+}
+
+
+
+// New Tiles
+
+const createBookTile = (book) => {
+    const bookTile = document.createElement("div")
+    const removeButton = document.createElement("button")
+    const title = document.createElement("p")
+    const author = document.createElement("p")
+    const pages = document.createElement("p")
+    const readStatus = document.createElement("div")
+    const readText = document.createElement("p")
+    const readBtn = document.createElement("button")
+
+    bookTile.classList.add("book-tile")
+    removeButton.classList.add("remove-button")
+    readStatus.classList.add("read-status")
+    readBtn.classList.add("read-button")
+
+    removeButton.textContent = "✖"
+    title.textContent = `${book.title}`
+    author.textContent = `${book.author}`
+    pages.textContent = `${book.pages} pages`
+
+    if (book.readStatus === true) {
+        readText.textContent = "Read"
+        readBtn.classList.add("green-status")
+    } else {
+        readText.textContent = "Unread"
+        readBtn.classList.add("red-status")
+    }
+
+    mainCont.appendChild(bookTile)
+    bookTile.appendChild(removeButton)
+    bookTile.appendChild(title)
+    bookTile.appendChild(author)
+    bookTile.appendChild(pages)
+    bookTile.appendChild(readStatus)
+    readStatus.appendChild(readText)
+    readStatus.appendChild(readBtn)
+}
